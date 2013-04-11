35269e0f84bed8e7b6401bf964f0d72b01abff38
maguirr4 <maguirr4@gmail.com>
2013-04-11 11:27:04 +0100

    Initial commit

---
+++ Calculator/src/Tests/CalcTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Ronan
+ *
+ */
+public class CalcTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ie.gcd.maths.Calc#Calc()}.
+	 */
+	@Test
+	public void testCalc() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ie.gcd.maths.Calc#add(int, int)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ie.gcd.maths.Calc#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Calculator/src/ie/gcd/maths/Calc.java
@@ -0,0 +1,17 @@
+package ie.gcd.maths;
+
+public class Calc {
+
+	public Calc(){
+		
+	}
+	
+	public int add(int x, int y){
+		return x-y;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
